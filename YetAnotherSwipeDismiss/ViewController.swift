d3fa29d507639f850cf8eaa644e57927752085ea
Pim <pim@pixelrock.nl>
2022-07-25 17:13:38 +0200

    Simpeler VC presenting

--- YetAnotherSwipeDismiss/ViewController.swift
+++ YetAnotherSwipeDismiss/ViewController.swift
@@ -8,21 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let viewControllers = [
+        UIViewController(),
+        SimpleViewController(),
+        StackViewController(),
+        TableViewController()
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(presentDismissViewController)))
-        
-        let viewControllers = [
-            UIViewController(),
-            SimpleViewController(),
-            StackViewController(),
-            TableViewController()
-        ]
-        
-        viewControllers.first?.view.backgroundColor = .green
         
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -39,45 +35,22 @@ class ViewController: UIViewController {
         
         viewControllers.forEach { viewController in
             var configuration = UIButton.Configuration.plain()
-            configuration.title = "\(type(of: viewController))"
+            configuration.title = "\(type(of: viewController.self))"
             let button = UIButton(configuration: configuration, primaryAction: UIAction { [unowned self] _ in
-                present(viewController, animated: true)
+                let newViewController = type(of: viewController).init()
+                if viewController == viewControllers.first {
+                    newViewController.view.backgroundColor = .green
+                }
+                present(newViewController, animated: true)
             })
             stackView.addArrangedSubview(button)
         }
-        //        present(emptyViewController, animated: true)
-                
-        //        let simpleViewController = SimpleViewController()
-        //        present(simpleViewController, animated: true)
-        //
-//                let stackViewController = StackViewController()
-//                present(stackViewController, animated: true)
-                
-        //        let tableViewController = TableViewController()
-        //        present(tableViewController, animated: true)
-        // TODO: Stack View with buttons for each view controller
         
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        presentDismissViewController()
-    }
-    
-    @objc func presentDismissViewController() {
-        
-//        let emptyViewController = UIViewController()
-//        present(emptyViewController, animated: true)
-        
-//        let simpleViewController = SimpleViewController()
-//        present(simpleViewController, animated: true)
-//
-//        let stackViewController = StackViewController()
-//        present(stackViewController, animated: true)
-        
-//        let tableViewController = TableViewController()
-//        present(tableViewController, animated: true)
-        
+        present(viewControllers[2], animated: true)
     }
 }
 
